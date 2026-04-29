3aadb18ed39fa5c799a8f51b0e6c4261c49920b1
TAEYUN CHOI <yooni0215@naver.com>
2026-04-30 05:36:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-window.addEventListener("load", () => {
-  document.querySelector(".page").classList.add("loaded");
-});
-
-const menuItems = document.querySelectorAll(".menu-item");
-
-menuItems.forEach((item) => {
-  item.addEventListener("mouseenter", () => {
-    document.body.classList.add("menu-hover");
-  });
-
-  item.addEventListener("mouseleave", () => {
-    document.body.classList.remove("menu-hover");
+document.querySelectorAll('.menu').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 });
